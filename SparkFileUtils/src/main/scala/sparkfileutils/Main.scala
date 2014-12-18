94855d8d3af61781af60d907a5924ed20d51938c
LDMtwo@gmail.com <LDMtwo@gmail.com@6ca776b7-01d2-cdfb-2498-feb3e7625287>
2014-12-18 06:24:19 +0000

    ???

--- SparkFileUtils/src/main/scala/sparkfileutils/Main.scala
+++ SparkFileUtils/src/main/scala/sparkfileutils/Main.scala
@@ -6,19 +6,23 @@
 
 package sparkfileutils
 import collection.JavaConversions._
+import org.apache.spark.storage.StorageLevel
 import scala.collection.JavaConversions._
 import filereorganizer.FileLineIndexer
 import filereorganizer.JTest
 import filereorganizer.Movie
 import java.io.File
+import java.util.ArrayList
 import java.util.HashSet
 import java.util.LinkedList
 import org.apache.spark._
+import scala.collection.mutable.ListBuffer
 
 object Main {
   def main(arg: Array[String]) {
     var m = new Movie("", "")
-
+    var NPARTS = arg(1).toInt
+    val DIR = arg(0)
     val conf = new SparkConf().setAppName("AppNameHere")
 
     val sc: SparkContext = new SparkContext(conf)
@@ -28,63 +32,81 @@ object Main {
     //}
 
     val rdd = sc.parallelize(new File(DIR).listFiles)
-    var rdd2 = rdd.map { f =>
-
+    var rdd2 = rdd
+    for( i <- Range(0, 3, 1)){
+    rdd2 = rdd2.map { f =>
       if (f.isDirectory) f.listFiles.toSeq
       else {
         Seq(f)
       }
     }.flatMap { identity }
-
-    while (true) {
-      
-      var rdd3 = rdd.map { f =>
-
-        if (f.isDirectory) f.listFiles.toSeq
-        else {
-          Seq(f)
-        }
-      }.flatMap { identity }.filter(f => f.isDirectory)
-      
-    }
-
-    var set = new HashSet[File]
-    var files = sc.makeRDD(set.toSeq)
-    var q = new LinkedList[Array[File]]
-    var i = 0
-    val DIR = arg(0)
-    (new File(DIR).listFiles).map { f =>
-      set.add(f)
-      if (f.isDirectory) q.add(f.listFiles)
-    }
-    var time = System.currentTimeMillis
-    //    var rt = Runtime.getRuntime
-    while (q.nonEmpty) {
-      try {
-        if (System.currentTimeMillis - time > 60000) {
-          FileLineIndexer.printMemory
-          JTest.printMemoryInfo(set.size, time)
-          time = System.currentTimeMillis
-          files = sc.makeRDD(set.toSeq)
-          files.saveAsTextFile("file:////mnt/hgfs/vmshare/files.out." + i + DIR.split('/').last)
-          i = i + 1
-          set.clear
-        }
-        val listOfArrays = (q.removeFirst).par.map { f =>
-          set.add(f)
-          if (f.isDirectory) q.add(f.listFiles)
-        }
-      } catch {
-        case e: Exception =>
-      }
-    }
-
-    println(set.size + " files")
-
-    files = sc.makeRDD(set.toSeq)
-    files.saveAsTextFile("file:////mnt/hgfs/vmshare/files.out." + (1) + DIR.split('/').last)
-
-    sc.stop()
   }
+  rdd2 = rdd2.repartition(NPARTS).persist(StorageLevel.DISK_ONLY)
+
+  var rdd3 = rdd2.map{ f =>
+    if (f.isDirectory) {
+
+      var list = new ArrayList[File]
+      var q = new LinkedList[Array[File]]
+      var i = 0
+      val DIR = arg(0)
+      (new File(DIR).listFiles).map { f =>
+        list.add(f)
+        if (f.isDirectory) q.add(f.listFiles)
+      }
+      while (q.nonEmpty) {
+        try {
+          val listOfArrays = (q.removeFirst).map { f =>
+            list.add(f)
+            if (f.isDirectory) q.add(f.listFiles)
+          }
+        } catch {
+          case e: Exception => e.printStackTrace
+        }
+      }
+      list.toSeq
+    } else {
+      Seq(f)
+    }
+  }.persist(StorageLevel.DISK_ONLY) //.flatMap { identity }.filter(f => f.isDirectory)
+
+  var rdd4 = rdd3.flatMap { identity }
+
+  //    var set = new HashSet[File]
+  //    var files = sc.makeRDD(set.toSeq)
+  //    var q = new LinkedList[Array[File]]
+  //    var i = 0
+  //    val DIR = arg(0)
+  //    (new File(DIR).listFiles).map { f =>
+  //      set.add(f)
+  //      if (f.isDirectory) q.add(f.listFiles)
+  //    }
+  //    var time = System.currentTimeMillis
+  //    //    var rt = Runtime.getRuntime
+  //    while (q.nonEmpty) {
+  //      try {
+  //        if (System.currentTimeMillis - time > 60000) {
+  //          FileLineIndexer.printMemory
+  //          JTest.printMemoryInfo(set.size, time)
+  //          time = System.currentTimeMillis
+  //          files = sc.makeRDD(set.toSeq)
+  //          files.saveAsTextFile("file:////mnt/hgfs/vmshare/files.out." + i + DIR.split('/').last)
+  //          i = i + 1
+  //          set.clear
+  //        }
+  //        val listOfArrays = (q.removeFirst).par.map { f =>
+  //          set.add(f)
+  //          if (f.isDirectory) q.add(f.listFiles)
+  //        }
+  //      } catch {
+  //        case e: Exception => e.printStackTrace
+  //      }
+  //    }
+  //
+//  println(rdd4.count + " files")
+  rdd4.saveAsTextFile("file:////mnt/hgfs/vmshare/files.out." + DIR.split('/').last)
+
+  sc.stop()
+}
 
 }
